9019b7b5428564d494ba12f387cd174a8933c56e
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2024-06-11 19:31:05 +0530

    github secrects added

--- src/main/java/com/shyam/controllers/DemoContoller.java
+++ src/main/java/com/shyam/controllers/DemoContoller.java
@@ -3,6 +3,7 @@ package com.shyam.controllers;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class DemoContoller {
     public Object sample() {
         return Map.of("name", "karnam shyam",
                       "email", "karnamshyam9009@gmail.com");
+    }
+
+    @GetMapping("/welcome/{name}")
+    public Object welcome(@PathVariable("name") String name) {
+        return "Welcome, " + name;
     }
 
 }
